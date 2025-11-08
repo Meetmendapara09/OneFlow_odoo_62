33eff5bd2b5835813191a188584b6ee27caa2c4c
Meet Mendapara <122281320+Meetmendapara09@users.noreply.github.com>
2025-11-08 10:23:30 +0530

    Add files via upload

---
+++ oneflow/src/main/java/com/example/oneflow/OneflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneflowApplication.class, args);
+	}
+
+}
---
+++ oneflow/src/test/java/com/example/oneflow/OneflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
